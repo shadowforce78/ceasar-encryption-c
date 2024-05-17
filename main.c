de4197280b77da6b6ed973b3c334f5f67bf52cdf
shadowforce78 <planque.adam@gmail.com>
2024-05-17 10:37:51 +0200

    refactor: Rename exo1.c to main.c and add file input for text and decalage

--- main.c
+++ main.c
@@ -3,18 +3,24 @@
 int main(int argc, char *argv[])
 {
     FILE *file = NULL;
+    FILE *file2 = NULL;
 
     int decalage;
+    char text[100];
+
+    printf("Entrez le texte a crypter : ");
+    scanf("%s", text);
+    file = fopen("text.txt", "w");
+    fprintf(file, "%s", text);
+    fclose(file);
 
     printf("Entrez le decalage : ");
     scanf("%d", &decalage);
 
     file = fopen("text.txt", "r");
-    FILE *file2 = NULL;
     file2 = fopen("text2.txt", "w");
     char c;
     while ((c = fgetc(file)) != EOF){
-        // Cesar encryption
         if (c >= 'A' && c <= 'Z'){
             c = c + decalage;
             if (c > 'Z'){
